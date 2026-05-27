b62df46a674e2e287b5332675eef06f418f61677
Jan Gabrielsson <jan@gabrielsson.com>
2026-05-27 12:03:27 +0200

    Add documentation for EventRunner7 features and release v0.1.14
    - Created forum posts for adding custom device properties, module system, and custom reactive objects.
    - Added HTML files for each topic with examples and usage instructions.
    - Implemented a copy-to-clipboard feature for easy sharing of forum posts.
    - Documented changes and enhancements in release v0.1.14, including error handling improvements.

--- EventRunner7.lua
+++ EventRunner7.lua
@@ -12,8 +12,16 @@ local function main(er)
 
   rule("@@00:00:05 => log('tick')",{check=false})
 
+  er.addStdProp("onIfOff",{
+  trigger = {type='device', property='value'},
+   get = function(_,id)
+      local value = fibaro.getValue(id,'value')
+      if type(value)=='boolean' then value = value and 1 or 0 end
+      if value == 0 then fibaro.call(id,"turnOn") end
+    end,
+    reduce = table.mapF
+})
 end
-
 function QuickApp:onInit()
   local str = "EventRunner 7, v"..fibaro.EventRunnerVersion
   self:debug(string.format("<font color='green'>%s</font>",str))
